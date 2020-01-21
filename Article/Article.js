0f530c6c1fe5f603ce8e193461a3365b62abe6eb
IT2 <it2@AWT.local>
2020-01-21 08:35:58 -0600

    wip

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,30 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+
+let createArticles = (title, date, firstParagraph, secondParagraph, thirdParagraph) => {
+
+  let articles = document.createElement('div');
+  let articleTitle = document.createElement('h2');
+  let articleDate = document.createElement('p');
+  let expandBtn = document.createElement('span');
+
+  console.log(articles)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  console.log('articles')
+  return null
+}
